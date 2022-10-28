37e0232ef0170872774449b29b7b93585ae53692
LucaConiglio <lucaconiglio.92@gmail.com>
2022-10-28 19:52:39 +0200

    terminato

--- js/main.js
+++ js/main.js
@@ -1,21 +1,40 @@
-const fullName = document.getElementById("full-name");
-const nomePasseggero = document.getElementById("nome-passeggero");
-const offerta = document.getElementById("sconto");
-
-const eta = document.getElementById("fascia-eta");
-let sceltaOption = eta.selectedIndex;
-
-console.log( fullName.value);
-
 const buttonGenera = document.getElementById("genera");
 
-buttonGenera.addEventListener( "click", function() {
+buttonGenera.addEventListener("click", function () {
+    const chilometri = document.getElementById ("chilometri");
+    const prezzo =parseInt(chilometri.value);
+    let prezzoTotale = prezzo * 0.21
+    const fullName = document.getElementById("full-name");
+    const nomePasseggero = document.getElementById("nome-passeggero");
+    const offerta = document.getElementById("sconto");
+    nomePasseggero.innerHTML = fullName.value;
+    const eta = document.getElementById("fascia-eta");
 
-nomePasseggero.innerHTML = fullName.value;
-
-offerta.innerHTML = eta.value;
+    
 
 
+    
+
+    const costoBiglietto = document.getElementById("costo")
+    if (eta.value === "20") {
+        prezzoTotale = prezzoTotale * 0.8;
+        costoBiglietto.innerHTML = `${prezzoTotale.toFixed(2)} €`
+        offerta.innerHTML = "20%";
+
+    } else if (eta.value === "40") {
+        prezzoTotale = prezzoTotale * 0.6;
+        costoBiglietto.innerHTML = `${prezzoTotale.toFixed(2)} €`
+        offerta.innerHTML = "40%";
+
+    } else {
+        costoBiglietto.innerHTML = `${prezzoTotale.toFixed(2)} €`;
+        offerta.innerHTML = "0%";
+    }
+})
 
 
+const buttonAnnulla = document.getElementById ("annulla");
+buttonAnnulla.addEventListener("click", function() {
+
+    alert("Ricarica la pagina Gentilmente e reinserisci i Dati");
 })
